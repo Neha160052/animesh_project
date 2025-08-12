27de5f64f520675544f861d46e3805f3acdd49af
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 17:15:46 +0530

    fixed the element collection error by changing the ProductReview  to  Set of collection

--- src/main/java/com/ttn/e_commerce_project/entities/Customer.java
+++ src/main/java/com/ttn/e_commerce_project/entities/Customer.java
@@ -3,6 +3,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -17,5 +19,5 @@ public class Customer {
 
     @OneToMany
     @JoinColumn(name = "customer_user_id",referencedColumnName = "user_id")
-    private ProductReview productReview;
+    private List<ProductReview> productReview;
 }
