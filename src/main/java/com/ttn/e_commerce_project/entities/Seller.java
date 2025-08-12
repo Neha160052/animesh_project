a2e0a30ecc63b49a495c6ed3401aaf04f18ac4b5
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 12:11:46 +0530

    created Seller Entity

---
+++ src/main/java/com/ttn/e_commerce_project/entities/Seller.java
@@ -0,0 +1,20 @@
+package com.ttn.demo_project.e_commerce_project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Seller
+{
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private String gst;
+    private Long companyContact;
+    private String companyName;
+}
