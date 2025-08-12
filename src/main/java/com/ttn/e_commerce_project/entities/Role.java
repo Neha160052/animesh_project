6d507f1c3a87a2ffa91d91be5e64a48cccb3e36c
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 14:16:31 +0530

    added user in a set to resolve mapping expected element collection/mapping  issue

--- src/main/java/com/ttn/e_commerce_project/entities/Role.java
+++ src/main/java/com/ttn/e_commerce_project/entities/Role.java
@@ -1,6 +1,8 @@
 package com.ttn.e_commerce_project.entities;
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Role {
 
@@ -13,5 +15,5 @@ public class Role {
   @JoinTable(name = "user_role",
           joinColumns = @JoinColumn(name ="user_id"),inverseJoinColumns = @JoinColumn(name = "role_id")
   )
-  private User user;
+  private Set<User> users;
 }
