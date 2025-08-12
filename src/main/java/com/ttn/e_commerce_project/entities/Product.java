b9dc31d8659721f617ace81770254bbfb2df24ca
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 17:03:38 +0530

    created

---
+++ src/main/java/com/ttn/e_commerce_project/entities/Product.java
@@ -0,0 +1,31 @@
+package com.ttn.e_commerce_project.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level= AccessLevel.PRIVATE)
+public class Product {
+
+       @Id
+       @GeneratedValue(strategy = GenerationType.IDENTITY)
+       Long id;
+       String name;
+       String description;
+       boolean isCancellable;
+       boolean isReturnable;
+       String brand;
+       boolean isActive;
+       boolean isDeleted;
+       @OneToMany
+       @JoinColumn(name = "Product_id",referencedColumnName = "id")
+       Set<ProductVariation> productVariation;
+
+ }
