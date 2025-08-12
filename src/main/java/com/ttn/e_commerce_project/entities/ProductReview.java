ec4058d139f04ee8b496d4edf4ab54e3e508d57a
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 17:39:05 +0530

    created ProductReview entity

---
+++ src/main/java/com/ttn/e_commerce_project/entities/ProductReview.java
@@ -0,0 +1,23 @@
+package com.ttn.e_commerce_project.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String review;
+    public enum rating{
+        ONE,TWO,THREE,FOUR,FIVE
+    }
+
+}
