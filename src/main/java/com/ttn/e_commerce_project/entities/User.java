07b7429e1a1d7f0a08faa6c3a4f2a7d9490180ba
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-12 12:11:25 +0530

    created user Entity

---
+++ src/main/java/com/ttn/e_commerce_project/entities/User.java
@@ -0,0 +1,37 @@
+package com.ttn.demo_project.e_commerce_project.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String firstName;
+   private String middleName;
+   private String lastName;
+   private String password;
+   private boolean isDeleted;
+   private boolean isActive;
+   private boolean isExpired;
+   private boolean isLocked;
+   private int invalidAttemptCount;
+   private Date passwordUpdateDate;
+
+   @OneToOne(mappedBy = "user")
+   private Seller seller;
+
+   @OneToOne(mappedBy = "user")
+   private Customer customer;
+
+   @ManyToMany
+   private Role role;
+
+}
