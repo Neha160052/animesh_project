36f54284540624f60e2c606fae352f67be43cf5a
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-13 11:48:48 +0530

    created entity cart

---
+++ src/main/java/com/ttn/e_commerce_project/entities/Cart.java
@@ -0,0 +1,21 @@
+package com.ttn.e_commerce_project.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    long quantity;
+    boolean isWishlistItem;
+
+}
