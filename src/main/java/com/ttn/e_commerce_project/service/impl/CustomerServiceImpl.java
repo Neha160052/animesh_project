1a700def89537f4f94196ee01c1d298acde198ea
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-19 15:24:58 +0530

    service class for customer

---
+++ src/main/java/com/ttn/e_commerce_project/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,56 @@
+package com.ttn.e_commerce_project.service.impl;
+
+import com.ttn.e_commerce_project.dto.co.CustomerCo;
+import com.ttn.e_commerce_project.entity.user.Customer;
+import com.ttn.e_commerce_project.entity.user.Role;
+import com.ttn.e_commerce_project.entity.user.User;
+import com.ttn.e_commerce_project.respository.CustomerRepository;
+import com.ttn.e_commerce_project.respository.RoleRepository;
+import com.ttn.e_commerce_project.respository.UserRepository;
+import com.ttn.e_commerce_project.service.CustomerService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CustomerServiceImpl implements CustomerService {
+
+     final UserRepository userRepository;
+     final CustomerRepository customerRepository;
+     final RoleRepository roleRepository;
+     final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public ResponseEntity<String> register(CustomerCo customerCo) {
+
+        if (userRepository.existsByEmail(customerCo.getEmail())) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+        if (!customerCo.getPassword().equals(customerCo.getConfirmPassword())) {
+            throw new IllegalArgumentException("Passwords do not match");
+        }
+        User user = new User();
+        Set<Role> customerRole = roleRepository.findByAuthority("Role_Customer");
+        user.setEmail(customerCo.getEmail());
+        user.setPassword(passwordEncoder.encode(customerCo.getPassword()));
+        user.setFirstName(customerCo.getFirstName());
+        user.setMiddleName(customerCo.getMiddleName());
+        user.setLastName(customerCo.getLastName());
+        user.setRole(customerRole);
+        userRepository.save(user);
+        Customer customer = new Customer();
+        customer.setUser(user);
+        customer.setContact(customerCo.getPhoneNumber());
+        customerRepository.save(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer registered successfully");
+    }
+}
