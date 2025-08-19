7166f9a4ff035be3ecbcf62c4866cbd1cf7dcb55
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-19 17:39:44 +0530

    added missing annotations in this class

--- src/main/java/com/ttn/e_commerce_project/util/MapToJsonConverter.java
+++ src/main/java/com/ttn/e_commerce_project/util/MapToJsonConverter.java
@@ -3,10 +3,12 @@ package com.ttn.e_commerce_project.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Converter
 public class MapToJsonConverter implements AttributeConverter<Map<String,Object>,String> {
 
     final ObjectMapper objectMapper = new ObjectMapper();
