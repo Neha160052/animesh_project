24fab92f6dc0ede4b499b4de4332c26cb7630407
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-19 15:18:00 +0530

    created controller for user

---
+++ src/main/java/com/ttn/e_commerce_project/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.ttn.e_commerce_project.controller;
+
+import com.ttn.e_commerce_project.dto.co.CustomerCo;
+import com.ttn.e_commerce_project.service.impl.CustomerServiceImpl;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+@RequestMapping("/register")
+public class UserController {
+
+    final CustomerServiceImpl customerService;
+
+    @PostMapping("/Customer")
+    public ResponseEntity<String> registerCustomer(@Valid @RequestBody CustomerCo customerCo)
+    {
+       return customerService.register(customerCo) ;
+    }
+}
+
