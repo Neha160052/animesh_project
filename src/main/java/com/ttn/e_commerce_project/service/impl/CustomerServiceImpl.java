54b991cc636458f874da27ddbc35ae52db061be2
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 09:27:00 +0530

    triggered email service with activation token for account activation

--- src/main/java/com/ttn/e_commerce_project/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/ttn/e_commerce_project/service/impl/CustomerServiceImpl.java
@@ -1,13 +1,16 @@
 package com.ttn.e_commerce_project.service.impl;
 
 import com.ttn.e_commerce_project.dto.co.CustomerCo;
+import com.ttn.e_commerce_project.entity.VerificationToken;
 import com.ttn.e_commerce_project.entity.user.Customer;
 import com.ttn.e_commerce_project.entity.user.Role;
 import com.ttn.e_commerce_project.entity.user.User;
+import com.ttn.e_commerce_project.enums.RoleAuthority;
 import com.ttn.e_commerce_project.respository.CustomerRepository;
 import com.ttn.e_commerce_project.respository.RoleRepository;
 import com.ttn.e_commerce_project.respository.UserRepository;
 import com.ttn.e_commerce_project.service.CustomerService;
+import com.ttn.e_commerce_project.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -19,13 +22,15 @@ import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class CustomerServiceImpl implements CustomerService {
 
-     final UserRepository userRepository;
-     final CustomerRepository customerRepository;
-     final RoleRepository roleRepository;
-     final PasswordEncoder passwordEncoder;
+      UserRepository userRepository;
+      CustomerRepository customerRepository;
+      RoleRepository roleRepository;
+      PasswordEncoder passwordEncoder;
+      VerificationTokenServiceImpl verificationTokenService;
+      EmailService emailService;
 
 
     @Override
@@ -38,18 +43,22 @@ public class CustomerServiceImpl implements CustomerService {
             throw new IllegalArgumentException("Passwords do not match");
         }
         User user = new User();
-        Set<Role> customerRole = roleRepository.findByAuthority("Role_Customer");
+        Role customerRole = roleRepository.findByAuthority(RoleAuthority.CUSTOMER).get();
         user.setEmail(customerCo.getEmail());
         user.setPassword(passwordEncoder.encode(customerCo.getPassword()));
         user.setFirstName(customerCo.getFirstName());
         user.setMiddleName(customerCo.getMiddleName());
         user.setLastName(customerCo.getLastName());
-        user.setRole(customerRole);
+        user.setRole(Set.of(customerRole));
         userRepository.save(user);
         Customer customer = new Customer();
         customer.setUser(user);
         customer.setContact(customerCo.getPhoneNumber());
         customerRepository.save(customer);
+        VerificationToken token = verificationTokenService.createToken(user);
+        String activationLink = "http://localhost:8080/activate?token=" + token.getToken();
+        emailService.sendJavaActivationEmail(user.getEmail(),activationLink);
         return ResponseEntity.ok("Customer registered successfully");
     }
+
 }
