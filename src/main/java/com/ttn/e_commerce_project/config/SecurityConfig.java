0da4721d0d867c2d897d21a1961bead1c395c720
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 09:28:15 +0530

    defined the security configurations for project

---
+++ src/main/java/com/ttn/e_commerce_project/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.ttn.e_commerce_project.config;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SecurityConfig{
+
+//    @Autowired
+//    JwtFilter jwtFilter;
+//
+//    @Autowired
+//    UserServiceImpl userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(request->request
+                .requestMatchers("/register/**","/activate/**").permitAll()
+                .requestMatchers("/customer/login","/seller/login").authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class) // generate token filter
+//                .addFilterAfter(jwtValidationFilter, JWTAuthenticationFilter.class) // validate token filter
+//                .addFilterAfter(jwtRefreshFilter, JwtValidationFilter.class) // refresh token filter
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
