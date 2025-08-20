199fe605076536d750545977fce8a76c13a0e1c8
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-21 00:00:05 +0530

    added functions to fetch seller gst and name from db

--- src/main/java/com/ttn/e_commerce_project/respository/SellerRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/SellerRepository.java
@@ -4,4 +4,8 @@ import com.ttn.e_commerce_project.entity.user.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SellerRepository extends JpaRepository<Seller,Long> {
+
+    boolean existsByGst(String gst);
+
+    boolean existsByCompanyNameIgnoreCase(String companyName);
 }
