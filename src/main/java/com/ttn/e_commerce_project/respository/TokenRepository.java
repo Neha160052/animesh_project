82c15dc362784968e3273259bfe14f8f892fbc45
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-21 01:12:32 +0530

    organise imports

--- src/main/java/com/ttn/e_commerce_project/respository/TokenRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/TokenRepository.java
@@ -1,6 +1,8 @@
 package com.ttn.e_commerce_project.respository;
 
 import com.ttn.e_commerce_project.entity.VerificationToken;
+import com.ttn.e_commerce_project.entity.user.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
