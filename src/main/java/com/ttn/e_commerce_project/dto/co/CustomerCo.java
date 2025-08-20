322ba7828a4bed6117243a88dd9bd4dc27ab77b7
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 20:22:19 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/dto/co/CustomerCo.java
+++ src/main/java/com/ttn/e_commerce_project/dto/co/CustomerCo.java
@@ -1,7 +1,12 @@
 package com.ttn.e_commerce_project.dto.co;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
