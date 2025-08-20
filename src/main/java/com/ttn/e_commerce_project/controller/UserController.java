a7b71560956e7f8f2d289d3900ba5eea1ed9f747
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 20:11:34 +0530

    injected  sellerservice

--- src/main/java/com/ttn/e_commerce_project/controller/UserController.java
+++ src/main/java/com/ttn/e_commerce_project/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     CustomerServiceImpl customerService;
+    SellerServiceImpl sellerService;
 
     @PostMapping("/customer")
     public ResponseEntity<String> registerCustomer(@Valid @RequestBody CustomerCo customerCo) {
