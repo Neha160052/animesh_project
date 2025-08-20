73e646f497dd71bfbbb481bb0274f8365d27dca9
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 09:19:07 +0530

    added the findByAuthority method in role repo and organised imports in customer repo

--- src/main/java/com/ttn/e_commerce_project/respository/CustomerRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.ttn.e_commerce_project.respository;
 
-import com.ttn.e_commerce_project.dto.co.CustomerCo;
 import com.ttn.e_commerce_project.entity.user.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
-    String register(CustomerCo customerCo);
 }
--- src/main/java/com/ttn/e_commerce_project/respository/RoleRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/RoleRepository.java
@@ -1,13 +1,13 @@
 package com.ttn.e_commerce_project.respository;
 
 import com.ttn.e_commerce_project.entity.user.Role;
+import com.ttn.e_commerce_project.enums.RoleAuthority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
-     Set<Role> findByAuthority(String authority);
+     Optional<Role> findByAuthority(RoleAuthority authority);
 }
