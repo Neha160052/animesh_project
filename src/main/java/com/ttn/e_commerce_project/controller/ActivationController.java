f40c6b7d8f424ea95f397195704a77db13ef6616
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 20:16:11 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/controller/ActivationController.java
+++ src/main/java/com/ttn/e_commerce_project/controller/ActivationController.java
@@ -9,10 +9,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
