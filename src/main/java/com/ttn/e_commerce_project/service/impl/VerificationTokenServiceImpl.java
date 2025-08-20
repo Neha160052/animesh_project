7465bbd227ebef4c6f4978da0e41017653aab031
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 20:10:04 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/service/impl/VerificationTokenServiceImpl.java
+++ src/main/java/com/ttn/e_commerce_project/service/impl/VerificationTokenServiceImpl.java
@@ -8,7 +8,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
