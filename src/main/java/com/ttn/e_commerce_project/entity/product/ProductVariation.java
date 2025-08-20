895ef3dcd92ade9c73b9015a61f633bcb1599a66
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-20 20:18:19 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/entity/product/ProductVariation.java
+++ src/main/java/com/ttn/e_commerce_project/entity/product/ProductVariation.java
@@ -9,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.annotations.ConverterRegistrations;
 
 import java.util.List;
 import java.util.Map;
