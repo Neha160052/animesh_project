43c8366f62d41c2b2ec4a44808d6ae9292ef2d30
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-21 23:55:27 +0530

    added the login functionality

---
+++ src/main/java/com/ttn/e_commerce_project/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.ttn.e_commerce_project.controller;
+
+import com.ttn.e_commerce_project.dto.co.UserLoginCo;
+import com.ttn.e_commerce_project.service.impl.CustomerServiceImpl;
+import com.ttn.e_commerce_project.service.impl.UserCommonService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+@RequiredArgsConstructor
+public class AuthController {
+
+    UserCommonService userCommonService;
+
+      @PostMapping("/login")
+      public String login(@Valid @RequestBody UserLoginCo userLoginCo)
+      {
+           return userCommonService.login(userLoginCo);
+      }
+}
---
+++ src/main/java/com/ttn/e_commerce_project/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.ttn.e_commerce_project.util;
+
+import com.ttn.e_commerce_project.entity.user.CustomUserDetails;
+import com.ttn.e_commerce_project.service.impl.UserDetailServiceImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JwtUtil {
+
+
+    final Key secretKey;
+
+
+    final long expiryMins=1000*60*10;
+
+    public JwtUtil(@Value("${secret.key}")String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateAccessToken(CustomUserDetails userDetails)
+    {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList());
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiryMins))//in milliseconds
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
