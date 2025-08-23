2a50af8fc2db3c2f66744a7a63f097126c7882b8
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-23 14:18:56 +0530

    added the login functionality

--- src/main/java/com/ttn/e_commerce_project/service/impl/UserCommonService.java
+++ src/main/java/com/ttn/e_commerce_project/service/impl/UserCommonService.java
@@ -21,4 +21,64 @@ public class UserCommonService {
         return roleRepository.findByAuthority(authority)
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("No role found for authority : %s", authority)));
     }
+
+    public AuthTokenVo login(UserLoginCo userLoginCo) {
+        log.info("hello i am running");
+
+        User user = userRepository.findByEmail(userLoginCo.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        if(!user.isActive())
+            throw  new AccountNotActiveException("Account not active");
+
+        if (user.isLocked()) {
+            if (unlockWhenTimeExpired(user)) {
+                user.setLocked(false);
+                user.setInvalidAttemptCount(0);
+                userRepository.save(user);
+            } else {
+                throw new AccountLockedException("Account is locked. Please try again after sometime");
+            }
+        }
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginCo.getEmail(),userLoginCo.getPassword()));
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            resetFailedAttempts(user);
+            String token = jwtUtil.generateAccessToken(userDetails);
+            String refreshToken = jwtUtil.generateRefreshToken(userDetails);
+            return AuthTokenVo.builder().refreshToken(refreshToken).accessToken(token).build();
+        } catch (BadCredentialsException e) {
+            increaseFailedAttempts(user);
+            throw new BadCredentialsException("You have reached the maximum limit of login attempts");
+        }
+    }
+
+    public void increaseFailedAttempts(User user) {
+        int invalidAttempts = user.getInvalidAttemptCount()+1;
+        user.setInvalidAttemptCount(invalidAttempts);
+
+        if(invalidAttempts >= 3)
+        {
+            user.setLocked(true);
+            user.setLockTime(LocalDateTime.now());
+            userRepository.save(user);
+        }
+    }
+//  To Do : send email to the user to stating that the account has been locked
+
+    public void resetFailedAttempts(User user) {
+        user.setInvalidAttemptCount(0);
+        userRepository.save(user);
+    }
+
+    public boolean unlockWhenTimeExpired(User user) {
+        if(user.getLockTime()==null)
+            return false;
+        LocalDateTime unlockTime = user.getLockTime().plusSeconds(Long.parseLong(lockTimeDuration));
+      if(LocalDateTime.now().isAfter(unlockTime))
+          return true;
+      else
+          return false;
+    }
 }
