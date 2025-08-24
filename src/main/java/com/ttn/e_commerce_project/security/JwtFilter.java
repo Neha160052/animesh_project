22f71df9aa2985a53f1f25b7bc05e455081a8084
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-24 17:14:54 +0530

    added the jwt filer to validate the access and refresh token

---
+++ src/main/java/com/ttn/e_commerce_project/security/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.ttn.e_commerce_project.security;
+
+import com.ttn.e_commerce_project.service.impl.JwtService;
+import com.ttn.e_commerce_project.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static java.util.Collections.singletonList;
+import static java.util.Objects.nonNull;
+
+
+@RequiredArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE,makeFinal = true)
+public class JwtFilter extends OncePerRequestFilter {
+
+    JwtService jwtService;
+
+    public static final String BEARER_HEADER = "Bearer ";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+        if (nonNull(authHeader) && authHeader.startsWith(BEARER_HEADER)) {
+            String jwt = authHeader.substring(7);
+            if (jwtService.validateToken(jwt)) {
+                String username = jwtService.getUsername(jwt);
+                String role = jwtService.getRoles(jwt);
+                var authorities = singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+                var auth = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
+
---
+++ src/main/java/com/ttn/e_commerce_project/service/impl/JwtService.java
@@ -0,0 +1,72 @@
+package com.ttn.e_commerce_project.service.impl;
+
+import com.ttn.e_commerce_project.exceptionhandling.InvalidArgumentException;
+import com.ttn.e_commerce_project.security.JwtFilter;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+public class JwtService {
+
+    Key secretKey;
+
+    public JwtService(@Value("${secret.key}")String secretKey)
+    {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)   // your secret key for verification
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Date expiration = claims.getExpiration();
+            if (expiration.before(new Date())) {
+                return true;
+            }
+            return false;
+
+        } catch (JwtException | IllegalArgumentException e) {
+            return true;
+        }
+
+    }
+
+    public String getUsername(String token) {
+
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return claims.getSubject(); // username is stored in subject
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidArgumentException("Invalid or expired token");
+        }
+    }
+    public String getRoles(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return claims.get("roles", List.class); // extract roles as List<String>
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new InvalidArgumentException("Invalid or expired token");
+        }
+    }
+
+}
