d193cd7288214e5abf55b4398cf94461435422e5
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-25 10:17:39 +0530

    added @service annotation

--- src/main/java/com/ttn/e_commerce_project/service/impl/JwtService.java
+++ src/main/java/com/ttn/e_commerce_project/service/impl/JwtService.java
@@ -7,11 +7,14 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Service
 public class JwtService {
 
     Key secretKey;
