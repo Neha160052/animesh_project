d7a2a58b8c731f8170e978776b08b111287e69ea
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-25 16:13:44 +0530

    added fields in CustomerVo

--- src/main/java/com/ttn/e_commerce_project/dto/vo/CustomerVo.java
+++ src/main/java/com/ttn/e_commerce_project/dto/vo/CustomerVo.java
@@ -1,4 +1,20 @@
 package com.ttn.e_commerce_project.dto.vo;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CustomerVo {
+     Long id;
+     String fullName;
+     @Email(message = "Enter a valid email address")
+     @NotBlank(message = "email is required")
+     String email;
+     boolean isActive;
 }
