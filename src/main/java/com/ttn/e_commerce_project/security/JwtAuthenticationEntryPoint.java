7567f5ee4495c9920ba99ab3f0cc51d02f673bf2
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-26 13:53:04 +0530

    created jwt authentication entry point to handle exception in case of expired jwt token

---
+++ src/main/java/com/ttn/e_commerce_project/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package com.ttn.e_commerce_project.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Token expired or invalid");
+    }
+}
