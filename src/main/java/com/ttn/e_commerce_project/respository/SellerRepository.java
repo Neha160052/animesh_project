331cff8d7f30c1bd93953da4bd58efdf0a077f55
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-26 17:33:49 +0530

    added querries for fetching the sellers details including the activation and deactivation of the same

--- src/main/java/com/ttn/e_commerce_project/respository/SellerRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/SellerRepository.java
@@ -8,4 +8,50 @@ public interface SellerRepository extends JpaRepository<Seller,Long> {
     boolean existsByGst(String gst);
 
     boolean existsByCompanyNameIgnoreCase(String companyName);
+
+    @Query("""
+       SELECT new com.ttn.e_commerce_project.dto.vo.SellerFlatVo(
+           u.id,
+           CONCAT(u.firstName, ' ', COALESCE(u.middleName, ''), ' ', u.lastName),
+           u.email,
+           u.isActive,
+           s.companyName,
+           s.companyContact,
+           a.city,
+           a.state,
+           a.country,
+           a.addressLine,
+           a.zipCode,
+           a.label
+       )
+       FROM Seller s
+       JOIN s.user u
+       JOIN u.address a
+       WHERE (:email IS NULL OR u.email = :email)
+       """)
+    Page<SellerFlatVo> findAllSellers(@Param("email")String email, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE User u
+    SET u.isActive = true
+    WHERE u.id = (
+        SELECT c.user.id FROM Seller c WHERE c.id = :sellerId
+    )
+    AND u.isActive = false
+""")
+    int activateSellerIfNotActive(@Param("sellerId") Long sellerId);
+
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE User u
+    SET u.isActive = false
+    WHERE u.id = (
+        SELECT s.user.id FROM Seller s WHERE s.id = :sellerId
+    )
+    AND u.isActive = true
+""")
+    int deactivateSellerIfActive(@Param("sellerId") Long sellerId);
 }
