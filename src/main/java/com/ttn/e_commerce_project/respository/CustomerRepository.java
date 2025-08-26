3c69c58d3ad20d8d8895df3f862a7e539e410171
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-26 17:30:19 +0530

    added querries for activation and deactivation of the customer

--- src/main/java/com/ttn/e_commerce_project/respository/CustomerRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/CustomerRepository.java
@@ -7,4 +7,40 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
+    @Query("""
+       SELECT new com.ttn.e_commerce_project.dto.vo.CustomerVo(
+           u.id,
+           CONCAT(u.firstName, ' ', COALESCE(u.middleName, ''), ' ', u.lastName),
+           u.email,
+           u.isActive
+       )
+       FROM Customer c
+       JOIN c.user u
+       WHERE (:email IS NULL OR u.email = :email)
+       """)
+  Page<CustomerVo> findAllCustomers(@Param("email") String email, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE User u
+    SET u.isActive = true
+    WHERE u.id = (
+        SELECT c.user.id FROM Customer c WHERE c.id = :customerId
+    )
+    AND u.isActive = false
+""")
+    int activateCustomerIfNotActive(@Param("customerId") Long customerId);
+
+  @Modifying
+  @Transactional
+  @Query("""
+    UPDATE User u
+    SET u.isActive = false
+    WHERE u.id = (
+        SELECT c.user.id FROM Customer c WHERE c.id = :customerId
+    )
+    AND u.isActive = true
+""")
+  int deactivateCustomerIfActive(@Param("customerId") Long customerId);
 }
