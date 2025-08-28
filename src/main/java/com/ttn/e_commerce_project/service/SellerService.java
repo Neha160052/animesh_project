5b22b6fb918361a91ac23a97ba0c6485e800204d
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-29 01:15:34 +0530

    added methods for seller service

--- src/main/java/com/ttn/e_commerce_project/service/SellerService.java
+++ src/main/java/com/ttn/e_commerce_project/service/SellerService.java
@@ -5,4 +5,8 @@ import com.ttn.e_commerce_project.dto.co.SellerCo;
 public interface SellerService {
 
     void register(SellerCo sellerCo);
+    SellerProfileVo getSellerProfile(String email);
+    void updateMyProfile(String email, @Valid SellerProfileCo req);
+    void updatePassword(String username, @Valid UpdatePasswordCo updatePasswordCo);
+    void updateAddress(String name, Long id, @Valid AddressCo addressCo);
 }
