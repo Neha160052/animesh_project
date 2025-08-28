2b578991060d4185145dd054c10d497d05368286
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-29 01:03:09 +0530

    orgnaised imports for customer service

--- src/main/java/com/ttn/e_commerce_project/service/CustomerService.java
+++ src/main/java/com/ttn/e_commerce_project/service/CustomerService.java
@@ -1,6 +1,13 @@
 package com.ttn.e_commerce_project.service;
 
 import com.ttn.e_commerce_project.dto.co.CustomerCo;
+import com.ttn.e_commerce_project.dto.co.CustomerProfileCo;
+import com.ttn.e_commerce_project.dto.co.UpdatePasswordCo;
+import com.ttn.e_commerce_project.dto.vo.AddressVo;
+import com.ttn.e_commerce_project.dto.vo.CustomerProfileVo;
+import jakarta.validation.Valid;
+
+import java.util.List;
 
 public interface CustomerService {
 
