78e97852543569c178f60733d04cd2ccf9e504ec
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-28 17:25:58 +0530

    added some function that are being used in the customer service

--- src/main/java/com/ttn/e_commerce_project/service/CustomerService.java
+++ src/main/java/com/ttn/e_commerce_project/service/CustomerService.java
@@ -5,5 +5,8 @@ import com.ttn.e_commerce_project.dto.co.CustomerCo;
 public interface CustomerService {
 
     void register(CustomerCo customerCo);
-
+    CustomerProfileVo getMyProfile(String email);
+    List<AddressVo> getMyAddresses(String email);
+    String updateMyProfile(String email, CustomerProfileCo customerProfileCo);
+    void updatePassword(String name, @Valid UpdatePasswordCo updatePasswordCo);
 }
