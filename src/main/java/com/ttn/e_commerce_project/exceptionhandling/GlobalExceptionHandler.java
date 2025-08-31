715e4c38193911315230619050204238c74e61ef
Animesh Yadav <animesh.yadav@tothenew.com>
2025-08-31 23:48:35 +0530

    added some more exceptions to be handle

--- src/main/java/com/ttn/e_commerce_project/exceptionhandling/GlobalExceptionHandler.java
+++ src/main/java/com/ttn/e_commerce_project/exceptionhandling/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.ttn.e_commerce_project.exceptionhandling;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
