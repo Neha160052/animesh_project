0e70303aa77bbf7683741aba7fed982bbf0175be
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-01 00:41:22 +0530

    added custom validator to validate the name fields in co's

---
+++ src/main/java/com/ttn/e_commerce_project/customvalidation/NameValidator.java
@@ -0,0 +1,18 @@
+package com.ttn.e_commerce_project.customvalidation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NameValidator  implements ConstraintValidator<ValidName, String> {
+
+    // Regex: Only letters upper,lower, spaces allowed but not leading/trailing or multiple
+    private static final String NAME_REGEX = "^[A-Za-z]+(?: [A-Za-z]+)*$";
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null || value.trim().isEmpty()) {
+            return false; // @NotBlank will also catch this
+        }
+        return value.matches(NAME_REGEX);
+    }
+}
---
+++ src/main/java/com/ttn/e_commerce_project/customvalidation/ValidName.java
@@ -0,0 +1,20 @@
+package com.ttn.e_commerce_project.customvalidation;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = NameValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidName {
+
+    String message() default "Invalid name format";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
