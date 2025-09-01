15b6ad55404c89b1ce2a15d5b8f1e43ed8a661b8
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-02 01:01:20 +0530

    added controller to add MetadataFields, get metadatafield and add category

--- src/main/java/com/ttn/e_commerce_project/controller/AdminController.java
+++ src/main/java/com/ttn/e_commerce_project/controller/AdminController.java
@@ -1,16 +1,26 @@
 package com.ttn.e_commerce_project.controller;
 
+import com.ttn.e_commerce_project.dto.co.CategoryCo;
+import com.ttn.e_commerce_project.dto.co.MetadataFieldCo;
+import com.ttn.e_commerce_project.dto.vo.CategoryVo;
 import com.ttn.e_commerce_project.dto.vo.CustomerVo;
+import com.ttn.e_commerce_project.dto.vo.MetadataFieldVo;
 import com.ttn.e_commerce_project.dto.vo.SellerVo;
-import com.ttn.e_commerce_project.service.impl.AdminService;
+import com.ttn.e_commerce_project.entity.category.Category;
+import com.ttn.e_commerce_project.entity.category.CategoryMetaDataField;
+import com.ttn.e_commerce_project.service.AdminService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.ttn.e_commerce_project.constants.UserConstants.*;
 
@@ -82,5 +92,30 @@ public class AdminController {
         } else {
             return ResponseEntity.ok(SELLER_ALREADY_DEACTIVATED + id);
         }
+    }
+// For a stable JSON structure, please use Spring Data's PagedModel (globally via @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO))
+//	or Spring HATEOAS and Spring Data's PagedResourcesAssembler as documented in https://docs.spring.io/spring-data/commons/reference/repositories/core-extensions.html#core.web.pageables.
+//Study more about this topic
+    @PostMapping("/add/metadata-fields")
+    public ResponseEntity<String> createMetadataField(@Valid @RequestBody MetadataFieldCo metadataFieldCo)
+    {
+        CategoryMetaDataField savedField = adminService.addMetaDataField(metadataFieldCo);
+        return ResponseEntity.ok(String.format(METADATA_FIELD_ADDED_SUCCESSFULLY,savedField.getId()));
+    }
+
+    @GetMapping("/get/metadata-fields")
+    public ResponseEntity<List<MetadataFieldVo>> getMetadataField( @RequestParam(defaultValue = "0") int offset,
+                                                             @RequestParam(defaultValue = "10") int max,
+                                                             @RequestParam(defaultValue = "id") String sort,
+                                                             @RequestParam(defaultValue = "ASC") String order,
+                                                             @RequestParam(required = false) String query)
+    {
+        List<MetadataFieldVo> fields = adminService.getAllMetadataFields(offset, max, sort, order, query).getContent();
+        return ResponseEntity.ok(fields);
+    }
+    @PostMapping("/add-category")
+    public ResponseEntity<CategoryVo> addCategory(@Valid @RequestBody CategoryCo categoryCo) {
+        CategoryVo categoryVo = adminService.addCategory(categoryCo);
+        return ResponseEntity.ok(categoryVo);
     }
 }
