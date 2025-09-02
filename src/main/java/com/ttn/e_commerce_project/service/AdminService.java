fe2175074fcedfb14195ce4faac4087c590803e3
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-02 12:15:24 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/service/AdminService.java
+++ src/main/java/com/ttn/e_commerce_project/service/AdminService.java
@@ -1,12 +1,6 @@
 package com.ttn.e_commerce_project.service;
 
-import com.ttn.e_commerce_project.dto.co.CategoryCo;
-import com.ttn.e_commerce_project.dto.co.MetadataFieldCo;
-import com.ttn.e_commerce_project.dto.vo.CategoryVo;
-import com.ttn.e_commerce_project.dto.vo.CustomerVo;
-import com.ttn.e_commerce_project.dto.vo.MetadataFieldVo;
-import com.ttn.e_commerce_project.dto.vo.SellerVo;
-import com.ttn.e_commerce_project.entity.category.CategoryMetaDataField;
+import com.ttn.e_commerce_project.dto.vo.*;
 import org.springframework.data.domain.Page;
 
 public interface AdminService {
