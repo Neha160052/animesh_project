211af9cfece5d5ee96c6d2b62015cfb18f5ea237
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-02 23:38:37 +0530

    added the category metadata field values co

---
+++ src/main/java/com/ttn/e_commerce_project/dto/co/CategoryMetaDataCo.java
@@ -0,0 +1,26 @@
+package com.ttn.e_commerce_project.dto.co;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@FieldDefaults(level =AccessLevel.PRIVATE)
+public class CategoryMetaDataCo {
+
+    @NotNull(message = "Category ID cannot be blank")
+    Long categoryId;
+
+    @NotNull(message = "Metadata Field ID cannot be blank")
+    Long metaDataFieldId;
+
+    @NotEmpty(message = "Field values cannot be blank")
+    List<String> fieldValues;
+
+}
