d33c4a95f1c1718a813af423fed0123dbe575b3d
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-02 15:43:24 +0530

    added audit aware impl to enable missing fields from the tables implementing auditing

---
+++ src/main/java/com/ttn/e_commerce_project/entity/audit/AuditAwareImpl.java
@@ -0,0 +1,19 @@
+package com.ttn.e_commerce_project.entity.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("AuditAware")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication==null)
+            return Optional.empty();
+        return Optional.of(authentication.getName());
+    }
+}
