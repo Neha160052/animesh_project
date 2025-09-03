93ccb0f35562ee01d58d7fc145cb35620a2da890
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-03 22:51:00 +0530

    refactored the function that was being used

--- src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.ttn.e_commerce_project.respository;
 
+import com.ttn.e_commerce_project.entity.category.Category;
 import com.ttn.e_commerce_project.entity.product.Product;
+import com.ttn.e_commerce_project.entity.user.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    boolean existsByNameAndBrandAndCategoryIdAndSellerId(String name,String brand,Long categoryId,Long sellerId);
+    boolean existsByNameAndBrandAndCategoryAndSeller(String name, String brand, Category category, Seller seller);
 }
