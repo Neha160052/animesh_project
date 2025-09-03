56f9f6b0771be3a109f950b9af259fbd2d7960ef
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-03 22:52:19 +0530

    added annotation jsoninclude non null

--- src/main/java/com/ttn/e_commerce_project/dto/vo/CategoryVo.java
+++ src/main/java/com/ttn/e_commerce_project/dto/vo/CategoryVo.java
@@ -1,13 +1,15 @@
 package com.ttn.e_commerce_project.dto.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CategoryVo {
-    
+
      Long id;
      String name;
      String message;
