e2462fb2a4350035bb017da5098d1011ccd670df
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-03 19:39:11 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/entity/category/CategoryMetaDataField.java
+++ src/main/java/com/ttn/e_commerce_project/entity/category/CategoryMetaDataField.java
@@ -1,13 +1,14 @@
 package com.ttn.e_commerce_project.entity.category;
 
 import com.ttn.e_commerce_project.entity.audit.Auditable;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-
-import java.util.Set;
 
 @Entity
 @Getter
