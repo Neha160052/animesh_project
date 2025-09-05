e02db9a8b500faf4f3ce4a6b62db32386d2421bd
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-05 16:22:53 +0530

    updated the validations for address

--- src/main/java/com/ttn/e_commerce_project/dto/co/AddressCo.java
+++ src/main/java/com/ttn/e_commerce_project/dto/co/AddressCo.java
@@ -4,6 +4,9 @@ import com.ttn.e_commerce_project.customvalidation.ValidName;
 import com.ttn.e_commerce_project.enums.Label;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,10 +16,20 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @FieldDefaults(level= AccessLevel.PRIVATE)
 public class AddressCo {
+
+    @ValidName(message="{city.name.invalid}")
+    @NotBlank
     String city;
+    @ValidName(message ="{state.name.invalid}")
+    @NotBlank
     String state;
+    @ValidName(message = "{country.name.invalid}")
+    @NotBlank(message = "address cannot be blank")
     String country;
+    @NotBlank(message = "address line cannot be left blank")
     String addressLine;
+    @NotNull(message = "zip code cannot be null")
+    @Pattern(regexp = "^\\d{6}$",message = "Enter a valid zip code")
     int zipCode;
     @Enumerated(EnumType.STRING)
     Label label;
