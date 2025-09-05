cf8c58e077c678fd7a2d38b87954794b7dea35f5
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-05 20:15:09 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
@@ -6,6 +6,7 @@ import com.ttn.e_commerce_project.entity.user.Seller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProductRepository extends JpaRepository<Product,Long> , JpaSpecificationExecutor<Product> {
     boolean existsByNameAndBrandAndCategoryAndSeller(String name, String brand, Category category, Seller seller);
