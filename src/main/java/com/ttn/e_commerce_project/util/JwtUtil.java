bc733682de6cc6bc43823a5ac35717c60d0773bf
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-06 12:47:00 +0530

    updated the token generation expiry time

--- src/main/java/com/ttn/e_commerce_project/util/JwtUtil.java
+++ src/main/java/com/ttn/e_commerce_project/util/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +27,6 @@ public class JwtUtil {
 
 
      Key secretKey;
-
 
      long expiryMins=1000*60*60;
      long refreshTokenExpiry = 1000 * 60 * 60 * 24 * 7;
@@ -46,7 +46,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiryMins))
+                .setExpiration(Date.from(Instant.now().plusSeconds(15*60*5)))
                 .setId(UUID.randomUUID().toString())
                 .claim("type", "access")
                 .signWith(secretKey)
