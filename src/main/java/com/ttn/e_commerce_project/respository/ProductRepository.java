0755b0f29ed0fc7edcb26433bcaff286f32a4d6c
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-08 19:46:38 +0530

    organised imports

--- src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
+++ src/main/java/com/ttn/e_commerce_project/respository/ProductRepository.java
@@ -9,6 +9,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> , JpaSpecificationExecutor<Product>,ProductRepositoryCustom{
     @Override
