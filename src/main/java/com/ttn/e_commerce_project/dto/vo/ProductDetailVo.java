86338981de68bcf6edf550d8162e4fdd167fe359
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-08 10:09:54 +0530

    added import for json include not null

--- src/main/java/com/ttn/e_commerce_project/dto/vo/ProductDetailVo.java
+++ src/main/java/com/ttn/e_commerce_project/dto/vo/ProductDetailVo.java
@@ -1,5 +1,6 @@
 package com.ttn.e_commerce_project.dto.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
